0a3f9dfb6d6fe3c66ba6f50f18c3888084362e53
Neefs <Gioven3207@icloud.com>
2024-05-14 10:59:23 -0400

    Add topPanel and websiteListPanel to Gui class

--- src/Gui.java
+++ src/Gui.java
@@ -3,14 +3,16 @@ import javax.swing.JPanel;
 
 public class Gui {
     JFrame frame;
-    JPanel mainPanel;
+    JPanel mainPanel, topPanel, websiteListPanel;
 
 
     public Gui() {
         frame = new JFrame("Password Manager");
         mainPanel = new JPanel();
+        topPanel = new JPanel();
+        websiteListPanel = new JPanel();
         frame.add(mainPanel);
-        frame.setSize(400, 400);
+        frame.setSize(1920, 1050);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
